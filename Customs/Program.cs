601d8bd8e05cf0cd9dc6b659ae4344922a041e02
Domingos Moitinho <domingos.sobrinho@outlook.com.br>
2020-05-14 16:12:57 -0300

    Adicionado Projeto Customs

---
+++ Customs/Program.cs
@@ -0,0 +1,51 @@
+﻿using Customs.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Customs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Commom, used or imported? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (ch == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else
+                {
+                    list.Add(new Product(name, price));
+                }   
+            }
+
+            Console.WriteLine("PRICE TAGS: ");
+
+            foreach (Product prod in list)
+            Console.WriteLine(prod.PriceTag());
+        }
+    }
+}
